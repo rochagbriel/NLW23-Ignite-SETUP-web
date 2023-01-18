395493a33fb959426a1abb7743438ee68f9d2c22
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-18 22:56:15 +0100

    adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import './styles/global.css';
 
-import { Habit } from "./components/Habit"
+// import { Habit } from "./components/Habit"
 
-function App() {
+import logoImage from './assets/logo.svg'
+
+export function App() {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={10} />
-      <Habit completed={20} />
-      <Habit completed={30} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        
+        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+          <img src="{logoImage}" alt="Habits" />
+
+          <button type="button">
+            New habit
+          </button>
+        </div>
+
+      </div>
     </div>
   )
 }
 
-export default App
+

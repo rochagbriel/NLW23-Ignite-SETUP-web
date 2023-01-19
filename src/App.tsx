a992261b8fc8aa5d317abf7ba290e9d84cfdfa33
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-19 19:37:44 +0100

    Head and Table pre-styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import './styles/global.css';
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
 // import { Habit } from "./components/Habit"
-
-import logoImage from './assets/logo.svg'
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        
-        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-          <img src= {logoImage} alt="Habits" />
-
-          <button type="button">
-            New habit
-          </button>
-        </div>
-
+        <Header />
+        <SummaryTable />
       </div>
     </div>
   )

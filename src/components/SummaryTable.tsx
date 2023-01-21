e9cf20a500ad4e8a3056c83d2e85de5eb2810668
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-21 16:57:29 +0100

    habitday and progress bar implemented

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -8,7 +8,7 @@ const summaryDates = generateDatesFromYearBeginning()
 const minimumSummaryDatesSize = 18 * 7 // 18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
-export function SummaryTable () {
+export function SummaryTable() {
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -26,7 +26,13 @@ export function SummaryTable () {
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(date => {
-                    return <HabitDay key={date.toString()}/>
+                    return (
+                        <HabitDay 
+                            key={date.toString()} 
+                            amount={5} 
+                            completed={Math.round(Math.random() * 5)} 
+                        />
+                    )
                 })}
 
                 {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {

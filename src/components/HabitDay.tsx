1d4e023f0f37073272561cdf662f4ca75958b558
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-21 16:28:33 +0100

    popover part 1 of 2

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,11 +1,25 @@
+import * as Popover from '@radix-ui/react-popover';
+import { ProgressBar } from './ProgressBar';
+
 // interface HabitDayProps {
 //     completed: number
 // }
 
 export function HabitDay() {
     return (
-        <div 
-        className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg">
-        </div>
+        <Popover.Root>
+            <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+            <Popover.Portal>
+                <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+                    <span className='font-sembold text-zinc-400'>Saturday</span>
+                    <span className='mt-1 font-extrabold leading-tight text-3xl'>Jan-21</span>
+
+                    <ProgressBar progress={0} />
+
+                    <Popover.Arrow height={8} width={16} className='fill-zinc-900'/>
+                </Popover.Content>
+            </Popover.Portal>
+        </Popover.Root>
     )
 }
